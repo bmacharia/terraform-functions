3da8b33f9da2f7990c2a375ca3fae50509e0adbe
Babu Macharia <babu.macharia@gmail.com>
2022-10-07 16:38:21 -0700

    Remove datetime folder out of filesystem folder

--- filesystem/datetime/timeadd/timeadd.tf
+++
@@ -1,9 +0,0 @@
-# timeadd adds a duration to a timestamp, returning a new timestamp
-
-locals {
-  timestamp = "2022-10-22T00:00:00Z"
-}
-
-output "timeadd" {
-  value = timeadd(local.timestamp, "1h")
-}
